81569dff3d40b4d3b16f82cf64d48a52c4721baa
Dulani-Viyathma <dulanii2002@gmail.com>
2025-04-12 14:09:20 +0530

    Editing sidebar icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export default function Sidebar({links}){
     return(
       <div className ="sidebar">
-        {links.map(link => (<a href="#!" key={link.name}>
+        {links.map(link => (<a className="sidebar-link" href="#!" key={link.name}>
           <FontAwesomeIcon icon={link.icon}/>
           {link.name}
 
