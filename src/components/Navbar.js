4eb262aab25c416b391bb514898d08be6869a89a
Dulani-Viyathma <dulanii2002@gmail.com>
2025-04-12 13:55:14 +0530

    Adding icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,42 @@
 import { useState } from "react";
+
+import Sidebar from "./Sidebar";
+import{faHome,faList,faCog}from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom";
 export default function Navbar() {
     const[showSidebar,setSidebar] = useState(false)
+    const links = [
+      {
+        name: "Home",
+        path: "/",
+        icon:faHome
+      },
+      {
+        name: "Recipes",
+        path: "/recipes",
+        icon:faList,
+      },
+      {
+        name: "Settings",
+        path: "/settings",
+        icon:faCog
+      }
+    ];
+    
+    
     return (
-      <div className="navbar container">
+        <>
+        <div className="navbar container">
         <a href="#!" className="logo">F<span>oo</span>diesHub</a>
         <div className="nav-links">
-            <a href="#!">Home</a>
+          {links.map(link =>(
+            <a href="#!" key={link.name}>{link.name}</a>
+
+          ))}
+          
+           {/* <a href="#!">Home</a>
             <a href="#!">Recipes</a>
-            <a href="#!">Settings</a>
+            <a href="#!">Settings</a>*/}
         </div>
         <div onClick={() => setSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
   <div className="bar"></div>
@@ -17,6 +46,11 @@ export default function Navbar() {
 
      
       </div>
+      <Sidebar links={links}/>
+
+
+        </>
+      
     );
   }
   
